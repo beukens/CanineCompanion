cfe5e4a670e46442d61ac4bfc3891fabebd1da5d
Beukens <beukensgaetan@gmail.com>
2024-03-04 11:44:32 +0100

    Implement search functionality in DogController
    Added a new public method, "search", to the DogController class which takes in a DogSearchForm and returns a ResponseEntity containing a list of DogShortDTO objects. This enhancement will allow users to search for dogs based on provided criteria.

--- src/main/java/be/yapock/caninecompanion/pl/controllers/DogController.java
+++ src/main/java/be/yapock/caninecompanion/pl/controllers/DogController.java
@@ -2,10 +2,12 @@ package be.yapock.caninecompanion.pl.controllers;
 
 import be.yapock.caninecompanion.bll.DogService;
 import be.yapock.caninecompanion.pl.models.dog.DogCreateForm;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import be.yapock.caninecompanion.pl.models.dog.DogSearchForm;
+import be.yapock.caninecompanion.pl.models.dog.DogShortDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/dog")
@@ -24,5 +26,18 @@ public class DogController {
     @PostMapping("/")
     public void createDog(@RequestBody DogCreateForm form) {
         dogService.create(form);
+    }
+
+    /**
+     * Searches for dogs based on the provided search criteria.
+     *
+     * @param form the search criteria form
+     * @return a ResponseEntity containing a list of DogShortDTO objects
+     */
+    @PostMapping("/search")
+    public ResponseEntity<List<DogShortDTO>> search(DogSearchForm form){
+        return ResponseEntity.ok(dogService.search(form).stream()
+                .map(DogShortDTO::fromEntity)
+                .toList());
     }
 }
