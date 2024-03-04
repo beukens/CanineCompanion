aa9a9cea06c4c0880dd23193d14536b802fef0e7
Beukens <beukensgaetan@gmail.com>
2024-03-04 10:42:25 +0100

    cc-27 #Front #COMMENT Add create method tests in DogServiceImplTest
    Implemented unit tests for the "create" method in the DogServiceImplTest class. These tests check the 'ok' scenario where a new dog entity is successfully created and saved into the repository and the 'ko' scenario where an IllegalArgumentException is thrown for a null form input.

---
+++ src/test/java/be/yapock/caninecompanion/bll/serviceImpls/DogServiceImplTest.java
@@ -0,0 +1,56 @@
+package be.yapock.caninecompanion.bll.serviceImpls;
+
+import be.yapock.caninecompanion.dal.models.Dog;
+import be.yapock.caninecompanion.dal.repositories.DogRepository;
+import be.yapock.caninecompanion.pl.models.dog.DogCreateForm;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class DogServiceImplTest {
+    @Mock
+    private DogRepository dogRepository;
+    @InjectMocks
+    private DogServiceImpl dogService;
+
+    private DogCreateForm createForm;
+    private Dog dog;
+
+    @BeforeEach
+    void setUp(){
+        createForm = new DogCreateForm("John", "Doe", LocalDate.now(), "Male", true);
+        dog = Dog.builder()
+                .firstName(createForm.firstName())
+                .lastName(createForm.lastName())
+                .dateOfBirth(createForm.dateOfBirth())
+                .sex(createForm.sex())
+                .isSterilized(createForm.isSterilized())
+                .build();
+    }
+
+    @Test
+    void create_ok() {
+        when(dogRepository.save(any(Dog.class))).thenReturn(dog);
+
+        dogService.create(createForm);
+
+        verify(dogRepository, times(1)).save(any(Dog.class));
+    }
+
+    @Test
+    void create_ko_formNull(){
+        Exception exception = assertThrows(IllegalArgumentException.class, ()-> dogService.create(null));
+
+        assertEquals("Le formulaire ne peut être vide", exception.getMessage());
+    }
+}
