2e156ca7edfb08e598cb255bbe09653314d5ec53
Beukens <beukensgaetan@gmail.com>
2024-03-04 16:13:47 +0100

    CC-71 #Front #COMMENT Extend DogCreateForm, add method to BreedService
    Extended the DogCreateForm class in DogServiceImplTest.java to include an additional field. Implemented a new "getAll" method in BreedServiceImplTest.java to retrieve all breeds from the repository; included corresponding tests.

--- src/test/java/be/yapock/caninecompanion/bll/serviceImpls/BreedServiceImplTest.java
+++ src/test/java/be/yapock/caninecompanion/bll/serviceImpls/BreedServiceImplTest.java
@@ -4,13 +4,20 @@ import be.yapock.caninecompanion.dal.models.Breed;
 import be.yapock.caninecompanion.dal.models.enums.DogSize;
 import be.yapock.caninecompanion.dal.models.enums.RaceGroup;
 import be.yapock.caninecompanion.dal.repositories.BreedRepository;
+import be.yapock.caninecompanion.pl.models.breed.BreedDTO;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class BreedServiceImplTest {
     @Mock
@@ -27,5 +34,14 @@ class BreedServiceImplTest {
                 .raceGroup(RaceGroup.GREYHOUND)
                 .temperament("temperament")
                 .build();
+    }
+
+    @Test
+    void getAll_ok(){
+        when(breedRepository.findAll()).thenReturn(Collections.singletonList(breed));
+
+        List<Breed> result = breedService.getAll();
+
+        assertEquals(Collections.singletonList(breed),result);
     }
 }
