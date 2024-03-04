8467effa9ef8714c7a2c0c7b1c2cdeeaed19dcca
Beukens <beukensgaetan@gmail.com>
2024-03-04 10:43:47 +0100

    Add DogService to DogController
    Added an instance of DogService into DogController and used it for constructor injection. This is a prerequisite to create a feasible interaction between the controller and service layer. As per this change, our DogController now has access to all the methods provided in DogService.

--- src/main/java/be/yapock/caninecompanion/pl/controllers/DogController.java
+++ src/main/java/be/yapock/caninecompanion/pl/controllers/DogController.java
@@ -1,9 +1,15 @@
 package be.yapock.caninecompanion.pl.controllers;
 
+import be.yapock.caninecompanion.bll.DogService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/dog")
 public class DogController {
+    private final DogService dogService;
+
+    public DogController(DogService dogService) {
+        this.dogService = dogService;
+    }
 }
