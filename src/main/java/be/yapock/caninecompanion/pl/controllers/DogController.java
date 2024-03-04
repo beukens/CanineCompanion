8d5f89a1b806f533b8840dcf4e632d43f713382d
Beukens <beukensgaetan@gmail.com>
2024-03-04 10:32:31 +0100

    CC-27 #back-testing #COMMENT Implement create method in DogService and DogController
    The DogService and DogController classes have both been enhanced with a create method for creating new dog entities. Corresponding changes have been made in the DogServiceImpl class and DogCreateForm class. Now, information about a new dog can be inputted via a form and is then saved to the dog repository.

--- src/main/java/be/yapock/caninecompanion/pl/controllers/DogController.java
+++ src/main/java/be/yapock/caninecompanion/pl/controllers/DogController.java
@@ -1,9 +1,28 @@
 package be.yapock.caninecompanion.pl.controllers;
 
+import be.yapock.caninecompanion.bll.DogService;
+import be.yapock.caninecompanion.pl.models.dog.DogCreateForm;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/dog")
 public class DogController {
+    private final DogService dogService;
+
+    public DogController(DogService dogService) {
+        this.dogService = dogService;
+    }
+
+    /**
+     * Creates a new dog with the provided information.
+     *
+     * @param form The form containing the information of the dog to be created.
+     */
+    @PostMapping("/")
+    public void create(@RequestBody DogCreateForm form) {
+        dogService.create(form);
+    }
 }
--- src/main/java/be/yapock/caninecompanion/pl/models/dog/DogCreateForm.java
+++ src/main/java/be/yapock/caninecompanion/pl/models/dog/DogCreateForm.java
@@ -1,11 +1,17 @@
 package be.yapock.caninecompanion.pl.models.dog;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record DogCreateForm(
+        @NotNull @NotBlank
         String firstName,
         String lastName,
+        @NotNull @NotBlank
         LocalDate dateOfBirth,
+        @NotNull @NotBlank
         String sex,
         boolean isSterilized
 ) {
