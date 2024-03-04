ea297502a2e8267a389d80a781395da66077c508
Beukens <beukensgaetan@gmail.com>
2024-03-04 13:05:19 +0100

    CC-31 #Front #COMMENT Implement update tests in DogServiceImplTest
    New tests have been introduced in the DogServiceImplTest class, primarily to verify the update function of the Dog Service. These tests ensure the correct handling of different scenarios such as successful updates, failure due to non-existent dog records, and when a null form is processed. Additionally, a minor text correction in exception messages has been made in the DogServiceImpl class.

--- src/test/java/be/yapock/caninecompanion/bll/serviceImpls/DogServiceImplTest.java
+++ src/test/java/be/yapock/caninecompanion/bll/serviceImpls/DogServiceImplTest.java
@@ -3,6 +3,7 @@ package be.yapock.caninecompanion.bll.serviceImpls;
 import be.yapock.caninecompanion.dal.models.Dog;
 import be.yapock.caninecompanion.dal.repositories.DogRepository;
 import be.yapock.caninecompanion.pl.models.dog.DogCreateForm;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -50,6 +52,32 @@ class DogServiceImplTest {
     @Test
     void create_ko_formNull(){
         Exception exception = assertThrows(IllegalArgumentException.class, ()-> dogService.create(null));
+
+        assertEquals("Le formulaire ne peut être vide", exception.getMessage());
+    }
+
+    @Test
+    void update_ok(){
+        when(dogRepository.findById(anyLong())).thenReturn(Optional.of(dog));
+        when(dogRepository.save(any())).thenReturn(dog);
+
+        dogService.update(createForm, anyLong());
+        verify(dogRepository, times(1)).save(any(Dog.class));
+    }
+
+    @Test
+    void update_ko_dogNotFound(){
+        long id = 1L;
+        when(dogRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Exception exception = assertThrows(EntityNotFoundException.class, ()-> dogService.update(createForm, 1L));
+
+        assertEquals("Le chien avec l'ID " + id + " n'existe pas", exception.getMessage());
+    }
+
+    @Test
+    void upddate_ko_FormNull(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> dogService.update(null, 1L));
 
         assertEquals("Le formulaire ne peut être vide", exception.getMessage());
     }
