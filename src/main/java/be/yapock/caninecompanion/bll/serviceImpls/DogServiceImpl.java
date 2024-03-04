66ec71cd3801e54b668fc083c06aeeb42e91d15f
Beukens <beukensgaetan@gmail.com>
2024-03-04 11:13:22 +0100

    CC-28 #back-testing #COMMENT Implement getDogById method in DogService
    Implemented a new method getDogById in DogService interface along with its corresponding implementation in DogServiceImpl. Further introduced a new route in DogController for finding a Dog based on its id. Also, a new DogFullDTO has been created to support the implementation.

--- src/main/java/be/yapock/caninecompanion/bll/serviceImpls/DogServiceImpl.java
+++ src/main/java/be/yapock/caninecompanion/bll/serviceImpls/DogServiceImpl.java
@@ -1,8 +1,10 @@
 package be.yapock.caninecompanion.bll.serviceImpls;
 
 import be.yapock.caninecompanion.bll.DogService;
+import be.yapock.caninecompanion.dal.models.Dog;
 import be.yapock.caninecompanion.dal.repositories.DogRepository;
 import be.yapock.caninecompanion.pl.models.dog.DogCreateForm;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,5 +18,17 @@ public class DogServiceImpl implements DogService {
     @Override
     public void create(DogCreateForm form) {
 
+    }
+
+    /**
+     * Retrieves a dog by its ID.
+     *
+     * @param id the ID of the dog to retrieve
+     * @return the dog with the specified ID
+     * @throws EntityNotFoundException if the dog is not found
+     */
+    @Override
+    public Dog getDogById(Long id) {
+        return dogRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("Chien pas trouvé"));
     }
 }
--- src/main/java/be/yapock/caninecompanion/pl/controllers/DogController.java
+++ src/main/java/be/yapock/caninecompanion/pl/controllers/DogController.java
@@ -1,6 +1,10 @@
 package be.yapock.caninecompanion.pl.controllers;
 
 import be.yapock.caninecompanion.bll.DogService;
+import be.yapock.caninecompanion.dal.models.Dog;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +15,17 @@ public class DogController {
 
     public DogController(DogService dogService) {
         this.dogService = dogService;
+    }
+
+    /**
+     * Retrieves a dog by its ID.
+     *
+     * @param id The ID of the dog to retrieve.
+     * @return The dog with the specified ID.
+     */
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN') || hasRole('ROLE_HELPER') || hasRole('ROLE_INTERN')")
+    public Dog getDogById(@PathVariable Long id) {
+        return dogService.getDogById(id);
     }
 }
