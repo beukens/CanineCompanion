53ec8d7e09c92d38bc7c4fc13133c62320cc3fb5
Beukens <beukensgaetan@gmail.com>
2024-03-18 13:08:12 +0100

    Update ExerciceServiceImplTest with getById tests
    Added tests for the new getById method in ExerciceServiceImplTest class. Two scenarios were covered: one where the Exercice object is successfully retrieved and another one where an EntityNotFoundException is thrown due to no Exercice found with the provided id.

--- src/test/java/be/yapock/caninecompanion/bll/serviceImpls/ExerciceServiceImplTest.java
+++ src/test/java/be/yapock/caninecompanion/bll/serviceImpls/ExerciceServiceImplTest.java
@@ -3,12 +3,15 @@ package be.yapock.caninecompanion.bll.serviceImpls;
 import be.yapock.caninecompanion.dal.models.Exercice;
 import be.yapock.caninecompanion.dal.repositories.ExerciceRepository;
 import be.yapock.caninecompanion.pl.models.exercice.ExerciceCreateForm;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.*;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -45,5 +48,19 @@ class ExerciceServiceImplTest {
     void create_ko(){
         Exception exception = assertThrows(IllegalArgumentException.class, ()-> exerciceService.create(null));
         assertEquals("Form ne peut être null", exception.getMessage());
+    }
+
+    @Test
+    void getOne_ok(){
+        when(exerciceRepository.findById(anyLong())).thenReturn(Optional.of(exercice));
+        Exercice result = exerciceService.getOneById(1L);
+        assertEquals(exercice, result);
+    }
+
+    @Test
+    void getOne_ko_notFound(){
+        when(exerciceRepository.findById(anyLong())).thenReturn(Optional.empty());
+        Exception exception = assertThrows(EntityNotFoundException.class, ()-> exerciceService.getOneById(anyLong()));
+        assertEquals("Exercice pas trouvé", exception.getMessage());
     }
 }
